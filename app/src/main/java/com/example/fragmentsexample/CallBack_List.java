e5c7a596c6122185939332bf9958f446f8756f54
Tom Cohen <Tomco@outlook.co.il>
2021-11-17 16:50:35 +0200

    Initial commit

---
+++ app/src/main/java/com/example/fragmentsexample/CallBack_List.java
@@ -0,0 +1,7 @@
+package com.example.fragmentsexample;
+
+public interface CallBack_List {
+    void setMainTitle(String str);
+    void setMainTitleColor(int color);
+    void rowSelected(int i);
+}
---
+++ app/src/main/java/com/example/fragmentsexample/CallBack_Map.java
@@ -0,0 +1,5 @@
+package com.example.fragmentsexample;
+
+public interface CallBack_Map {
+    void mapClicked(double lat, double lon);
+}
